5b79c31ece1f1ca3aefdd6e0582aa56f31cf34e1
Austin Long <46079987+alifelongwork@users.noreply.github.com>
2025-03-23 07:17:06 -0600

    shell script update
    was not looping through mice properly, fixed. 
    Running for too many iterations, shortened 1 to 10, was unsure of inclusion at end

--- Energy_Analysis/test_run.sh
+++ Energy_Analysis/test_run.sh
@@ -3,13 +3,13 @@
 DATA_LOAD_PATH="./Data"
 MODEL_SAVE_PATH="./Output/Model"
 STATS_SAVE_PATH="./Output/Stats"
-MICE=("cChR1" "fChR4" "DJC000" "DJC002" "fChR5" "fChR2")
+MICE=("fChR4" "DJC000" "DJC002" "fChR5" "fChR2" "cChR1")
 
 source /home/austin/Research_Projects/Inverse-Ising-solver-main/Abigail/Analysis/cluster_scripts/venv/bin/activate
 
-for mouse in $MICE
+for mouse in ${MICE[@]}
 do
-	for i in {1..11}
+	for i in {1..10}
 	do
 		python3 main_gen_multi_model.py --DATA_LOAD_FILE_DIR "$DATA_LOAD_PATH" --MODEL_SAVE_FILE_DIR "$MODEL_SAVE_PATH" --JOB_NUM "$i" --MOUSE "$mouse" --TIME_SEG "pre"
 		python3 main_gen_multi_model.py --DATA_LOAD_FILE_DIR "$DATA_LOAD_PATH" --MODEL_SAVE_FILE_DIR "$MODEL_SAVE_PATH" --JOB_NUM "$i" --MOUSE "$mouse" --TIME_SEG "mid"
